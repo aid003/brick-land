8fe9a59174c1a2958297578d9243f9fbe3d7fd87
aid003 <teremochek859@gmail.com>
2023-06-25 15:57:09 +0300

    stpd

---
+++ pages/src/components/data.js
@@ -0,0 +1,11 @@
+export default questions = [
+  {
+    questionText: "Выберите производителя",
+    answer: [
+      { answerText: "Что то там", chosen: false },
+      { answerText: "Что то там2", chosen: false },
+      { answerText: "Что то там3", chosen: false },
+      { answerText: "Что то там4", chosen: false },
+    ],
+  },
+];
---
+++ pages/src/components/utils/nodemailer.js
@@ -0,0 +1,15 @@
+import nodemailer from "nodemailer";
+
+export const transporter = nodemailer.createTransport({
+  service: "gmail",
+  secure: false,
+  auth: {
+    user: process.env.GMAIL_EMAIL_ADDRESS,
+    pass: process.env.GMAIL_APP_PASSWORD,
+  },
+});
+
+export const mailOptions = {
+  from: process.env.GMAIL_EMAIL_ADDRESS,
+  to: process.env.GMAIL_EMAIL_ADDRESS,
+};
